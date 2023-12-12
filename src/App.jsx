f8a5144bccc2f3338b34fcf64091621d35d5db97
Geoff George <geoffgeorgethommana@gmail.com>
2023-12-12 19:16:51 +0530

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/navbar'
+import { Box } from '@mui/material'
 
 function App() {
  
 
   return (
     <>
-        hi
+      <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
     </>
   )
 }
